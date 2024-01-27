2818b2d5fe362a5dd2e0a06c200a7c0c21c5854f
Fernando125 <fernandogomez125@gmail.com>
2024-01-27 14:41:41 +0100

    testy testy

--- deleteLogs.sh
+++ deleteLogs.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+# Directorio donde se encuentran los archivos
+DIRECTORIO="/ruta/del/directorio/de/archivos"
+
+# Encuentra y elimina archivos con más de 7 días de antigüedad
+find "$DIRECTORIO" -type f -mtime +7 -exec rm {} \;
+
+echo "Operación completada: Se han eliminado los archivos con más de 7 días de antigüedad."
