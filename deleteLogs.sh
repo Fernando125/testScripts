97b695933f68ed2989c0749a09d0e713bc785657
Fernando125 <fernandogomez125@gmail.com>
2024-01-27 19:57:08 +0100

    Modified deleteLogs.sh

--- deleteLogs.sh
+++ deleteLogs.sh
@@ -6,4 +6,4 @@ directorio="/ruta_del_directorio"
 # Encuentra y elimina los logs con mas de 7 dias
 find "$directorio" -type f -mtime +7 -exec rm {} \;
 
-echo "Operacion completada: Se han eliminado los logs con mas de 7 dias"
+echo "Se han eliminado los logs con mas de 7 dias"
