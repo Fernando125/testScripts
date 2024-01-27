24b7f40dd5fc9588c01505dcdbd5a5e333f35651
Fernando125 <30904924+Fernando125@users.noreply.github.com>
2024-01-27 19:53:39 +0100

    Delete deleteLogs.sh

--- deleteLogs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Directorio donde se encuentran los archivos
-directorio="/ruta_del_directorio"
-
-# Encuentra y elimina los logs con mas de 7 dias
-find "$directorio" -type f -mtime +7 -exec rm {} \;
-
-echo "Operacion completada: Se han eliminado los logs con mas de 7 dias"
