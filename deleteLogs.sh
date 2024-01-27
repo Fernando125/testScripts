11bb3c43ca75d45dcc83814227610333f2052d66
Fernando125 <fernandogomez125@gmail.com>
2024-01-27 18:50:00 +0100

    Code modified

--- deleteLogs.sh
+++ deleteLogs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Directorio del que se borraran los logs
-directorio="./"
+# Directorio donde se encuentran los archivos
+directorio="."
 
 # Encuentra y elimina archivos con más de 7 días de antigüedad
-find "$DIRECTORIO" -type f -mtime +7 -exec rm {} \;
+find "$directorio" -type f -mtime +7 -exec rm {} \;
 
+# Notifico al usuario que los logs de los ultimos 7 dias han sido eliminados
 echo "Se han borrado los logs de hace mas de 7 dias"
