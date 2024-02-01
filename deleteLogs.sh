50431d4835cd3290e2707690d875eb2429b14779
Profesional <Fernando.Gomez@externals.adidas.com>
2024-02-01 23:55:55 +0100

    Compress to .zip

--- deleteLogs.sh
+++ deleteLogs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Directorio donde se encuentran los archivos
-directorio="/ruta_del_directorio"
+directorio="/c/Users/Profesional/Documents/testScripts"
+fecha=$(date +'%d%m%Y')
 
 # Encuentra y elimina los logs con mas de 7 dias
 find "$directorio" -type f -mtime +7 -exec rm {} \;
+
+find "$directorio" -type f -name "*.log" -ctime -7 -exec zip logs-$fecha.zip {} +
 
 echo "Se han eliminado los logs con mas de 7 dias"
