1f16e8bc4d0cdb9c4b09bd833d0c876020a3ab94
Profesional <Fernando.Gomez@externals.adidas.com>
2024-02-01 23:58:50 +0100

    Fix path

--- deleteLogs.sh
+++ deleteLogs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Directorio donde se encuentran los archivos
-directorio="/c/Users/Profesional/Documents/testScripts"
+directorio="ruta_directorio"
 fecha=$(date +'%d%m%Y')
 
 # Encuentra y elimina los logs con mas de 7 dias
