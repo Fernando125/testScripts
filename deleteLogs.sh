fc32da91b563313fb47c58e7a19de9c1efd54ced
Fernando125 <30904924+Fernando125@users.noreply.github.com>
2024-02-03 11:41:06 +0100

    Update deleteLogs.sh

--- deleteLogs.sh
+++ deleteLogs.sh
@@ -5,7 +5,7 @@ directorio="ruta_directorio"
 fecha=$(date +'%d%m%Y')
 
 # Encuentra y elimina los logs con mas de 7 dias
-find "$directorio" -type f -mtime +7 -exec rm {} \;
+find "$directorio" -type f -ctime +7 -exec rm {} \;
 
 find "$directorio" -type f -name "*.log" -ctime -7 -exec zip -q logs_$fecha.zip *.log \;
 
