3a5c0b08d7264c655102d392232acb3f9cfabe2c
Fernando125 <30904924+Fernando125@users.noreply.github.com>
2024-02-03 21:10:23 +0100

    Update comments

--- deleteLogs.sh
+++ deleteLogs.sh
@@ -7,8 +7,10 @@ fecha=$(date +'%d%m%Y')
 # Encuentra y elimina los logs con mas de 7 dias
 find "$directorio" -type f -mtime +7 -exec rm {} \;
 
+# Encuentra los ficheros "*.log" con menos de 7 dias y los comprime en .zip
 find "$directorio" -type f -name "*.log" -ctime -7 -exec zip -q logs_$fecha.zip *.log \;
 
+# Se eliminan los logs que han sido comprimidos para que no haya informacion duplicada
 find "$directorio" -type f -name "*.log" -ctime -7 -exec rm {} \;
 
 echo "Se han comprimido los logs con menos de 7 días y se han eliminado los logs con mas de 7 dias"
