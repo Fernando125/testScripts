55a8b2e334ad7dc46a9b96e592cec0437f8c85f9
Fernando125 <fernandogomez125@gmail.com>
2024-03-16 19:51:37 +0100

    deleteLogs modified

--- deleteLogs.sh
+++ deleteLogs.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 # Directory where we can see the logs
-directory="path_directory"
+directory="/home/fernando/pruebas"
 # Variable to keep the date of the day in the format "01012024"
 dateLogs=$(date +'%d%m%Y')
 
+rename_logs() {
+    # Loop for rename all logs files
+    for file in "$directory"/*; do
+        # Get filename
+        filename=$(basename "$file")
+        filename_noext="${filename%.*}"
+
+        # New renamed filename
+        new_filename="${filename_noext}-1.log"
+    done
+}
+
 # Find and delete log older than 7 days
 find "$directory" -type f -mtime +7 -exec rm {} \;
+
+# Find all log fields older than 7 days, duplicate and rename them
+
+rename_logs
 
 # Finds "*.log" files less than 7 days old and compresses them into .zip
 find "$directory" -type f -name "*.log" -ctime -7 -exec zip -q logs_$dateLogs.zip *.log \;
